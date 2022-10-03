aeb6f32ae10731d551e3908413f496a62db1573c
Anish <113669810+radlysamar@users.noreply.github.com>
2022-10-03 15:10:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let name = "Anish"
+console.log(name)
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2, 4))
+console.log(name.slice(2))
+console.log(name.replace("sh", "sha"))
+
+let fri = "vansika"
+console.log(fri)
+let me = "Anish"
+console.log(fri.concat(" is a best friend of " ,me ," XD."))
+let frin = "    kumari     "
+console.log(frin)
+console.log(frin.trim())
+let friend ="rocky"+ "       vishaka    " 
+console.log(friend.trim()[0])
+console.log(friend.trim()[1])
+console.log(friend.trim()[2])
+console.log(friend.trim()[3])
+console.log(friend.trim()[4])
+console.log(friend.trim()[5])
+console.log(friend.trim()[6])
+console.log(friend)
+
+//quickquiz :use a "for loop"to print a string
+const alp = 'anish';
+for (const k of alp) {
+  console.log(k)
+}
